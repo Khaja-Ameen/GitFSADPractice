73f7dd3e7d125dc1082454d4b0634fe2c023b30e
Exam Practice <khaja.ameensk@gmail.com>
2026-01-06 14:47:05 +0530

    exp-3

---
+++ exp-2/src/main/java/com/cse/exp2/dao/ProductDAO.java
@@ -0,0 +1,52 @@
+package com.cse.exp2.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.cse.exp2.model.Product;
+import com.cse.exp2.util.HibernateUtil;
+
+public class ProductDAO {
+
+    // CREATE
+    public void addProduct(Product product) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(product);
+        tx.commit();
+        session.close();
+    }
+
+    // READ
+    public Product getProduct(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Product product = session.get(Product.class, id);
+        session.close();
+        return product;
+    }
+
+    // UPDATE
+    public void updateProduct(int id, double price, int quantity) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Product product = session.get(Product.class, id);
+        if (product != null) {
+            product.setPrice(price);
+            product.setQuantity(quantity);
+            session.update(product);
+        }
+        tx.commit();
+        session.close();
+    }
+
+    // DELETE
+    public void deleteProduct(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Product product = session.get(Product.class, id);
+        if (product != null) {
+            session.delete(product);
+        }
+        tx.commit();
+        session.close();
+    }
+}
