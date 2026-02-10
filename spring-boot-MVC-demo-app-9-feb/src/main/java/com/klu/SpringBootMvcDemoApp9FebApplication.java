8e567a2e4af931687783ee88d0d1032fce4c43a3
Exam Practice <khaja.ameensk@gmail.com>
2026-02-10 08:20:01 +0530

    10-2-2025

---
+++ spring-boot-MVC-demo-app-9-feb/src/main/java/com/klu/SpringBootMvcDemoApp9FebApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcDemoApp9FebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcDemoApp9FebApplication.class, args);
+	}
+
+}
